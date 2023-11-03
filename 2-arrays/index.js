7665da1ab13835c0d626e9e57e1b57c4812b853f
jessica deng <jdeng43@my.bcit.ca>
2023-11-03 15:22:50 -0700

    add question 1 and the answer

--- 2-arrays/index.js
+++ 2-arrays/index.js
@@ -9,9 +9,21 @@
 
 /**
  * Question 5 - concat two different arrays by using .concat() method
- * 
- *  
+
+ * initialise the variable called coins to an array containing the values:
+ * nickels, dimes
+ * initialise the variable called  more coins to an array containing the values:
+ * quarters, loonies, toonies
+ * with a variable array called total coins, add the array "more coins" to the end of "coins" using concatenation
+ * Then console log out total coins. Write a single line comment as to what will be displayed.
  */
+
+var coins = ["nickels", "dimes"];
+var moreCoins = ["quarters", "loonies", "toonies"];
+var totalCoins = coins.concat(moreCoins);
+
+console.log(totalCoins);
+//[ 'nickels', 'dimes', 'quarters', 'loonies', 'toonies' ]
 
 
 
